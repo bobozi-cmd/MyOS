91e759d3220aa3dd0749fef79b977f824e572af1
bobozi <1210611257@qq.com>
2024-02-14 09:47:35 -0800

    Add stack array version phyiscal memory management

---
+++ include/pmm.h
@@ -0,0 +1,38 @@
+#ifndef INCLUDE_PMM_H_
+#define INCLUDE_PMM_H_
+
+#include "multiboot.h"
+#include "types.h"
+
+// thread stack size
+#define STACK_SIZE 8192
+
+// Max physical memory (512 MB)
+#define PMM_MAX_SIZE 0x20000000
+
+// Physical Memory Frame size
+#define PMM_PAGE_SIZE 0x1000
+
+// Max physical page number
+#define PAGE_MAX_SIZE (PMM_MAX_SIZE/PMM_PAGE_SIZE)
+
+// page mask, 4k align
+#define PHY_PAGE_MASK 0xFFFFF000
+
+// kernel file's start and end addr in memory
+// defined in kernel.ld
+extern uint8_t kern_start[];
+extern uint8_t kern_end[];
+
+// dynamic allocated page number
+extern uint32_t phy_page_count;
+
+void show_memory_map();
+
+void init_pmm();
+
+uint32_t pmm_alloc_page();
+
+void pmm_free_page(uint32_t p);
+
+#endif // INCLUDE_PMM_H_
--- init/entry.c
+++ init/entry.c
@@ -6,6 +6,7 @@
 #include "vargs.h"
 #include "types.h"
 #include "debug.h"
+#include "pmm.h"
 
 int kern_entry() {
     init_debug();
@@ -18,7 +19,25 @@ int kern_entry() {
 
     init_timer(200);
 
-    asm volatile ("sti");
+    printk("kernel in memory start: 0x%x\n", kern_start);
+    printk("kernel in memory end:   0x%x\n", kern_end);
+    printk("kernel in memory used:   %d KB\n\n", (kern_end - kern_start + 1023) / 1024);    
+
+    show_memory_map();
+    init_pmm();
+
+    printk_color(rc_black, rc_red, "\nThe Count of Physical Memory Page is: %u\n\n", phy_page_count);
+
+    uint32_t allc_addr = NULL;
+    printk_color(rc_black, rc_light_brown, "Test Physical Memory Alloc :\n");
+    allc_addr = pmm_alloc_page();
+    printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+    allc_addr = pmm_alloc_page();
+    printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+    allc_addr = pmm_alloc_page();
+    printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
+    allc_addr = pmm_alloc_page();
+    printk_color(rc_black, rc_light_brown, "Alloc Physical Addr: 0x%08X\n", allc_addr);
 
     return 0;
 }
